ac050af70cc3f7c4f87061ce638a0850e8615ff2
Weswess <117919998+Weswess@users.noreply.github.com>
2024-04-09 13:14:04 +0200

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -56,7 +56,7 @@ questions = [
     {
         "question": "Who is known as the Father of Modern Physics?",
         "options": ["A: Terence Tao", "B: Steve hawkins", "C: Albert Einstein", "D: William James Sidis"],
-        "answer": "B"
+        "answer": "C"
     },
 ]
 
